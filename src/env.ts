8f3fa11c97f70bd06cef8795b46d578fb1b41530
Alexander Haas <mail@haas-alexander.com>
2023-05-12 12:18:12 +0200

    feat: :tada: Initial commit
    Basic setup with REST API and frontend code

---
+++ src/env.ts
@@ -0,0 +1,3 @@
+export const API_URL = import.meta.env.API_URL;
+
+console.log(API_URL);
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	let msg = '';
+
+	onMount(async () => {
+		msg = await (await fetch('/api/hello')).text();
+		console.log(msg);
+	});
+</script>
+
+<h1>Welcome to File Server</h1>
+<h2>
+	{msg}
+</h2>
